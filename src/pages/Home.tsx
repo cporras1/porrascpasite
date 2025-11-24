2c4c63ee297900f6fa4af28614047d2b8d55af66
cporras1 <email@cesarporras.com>
2025-11-23 18:44:36 -0700

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { HomeHeader } from '../components/HomeHeader';
 import { Hero } from '../components/Hero';
 import { About } from '../components/About';
@@ -7,6 +9,27 @@ import { Contact } from '../components/Contact';
 import { Footer } from '../components/Footer';
 
 export function Home() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const elementId = location.hash.substring(1);
+      const element = document.getElementById(elementId);
+      if (element) {
+        const headerHeight = 80;
+        const elementPosition = element.getBoundingClientRect().top + window.pageYOffset;
+        const offsetPosition = elementPosition - headerHeight;
+
+        setTimeout(() => {
+          window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth'
+          });
+        }, 100);
+      }
+    }
+  }, [location]);
+
   return (
     <div className="min-h-screen bg-white">
       <HomeHeader />
