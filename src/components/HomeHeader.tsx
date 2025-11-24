d3d05f620209ae91bdb2e7050f50e7c63c4633d2
cporras1 <email@cesarporras.com>
2025-11-23 20:32:32 -0700

    Updated index.css

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -26,7 +26,7 @@ export function HomeHeader() {
       } else {
         const element = document.querySelector(href);
         if (element) {
-          const headerHeight = 200;
+          const headerHeight = 250;
           const elementPosition = element.getBoundingClientRect().top + window.pageYOffset;
           const offsetPosition = elementPosition - headerHeight;
 
